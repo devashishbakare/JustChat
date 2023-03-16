56ebad58831ba83522a44ae9ddd358f34e4d0eba
devashish bakare <devashishbakare@gmail.com>
2023-03-16 12:58:26 +0530

    added home and chat compoent and fetch data from backend to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
-
+import { Route } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import ChatPage from './Pages/ChatPage';
 function App() {
   return (
     <div className="App">
       first App render
+      <Route exact path ="/" component={HomePage}></Route>
+      <Route path ="/chats" component={ChatPage}></Route>
     </div>
   );
 }
---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {useState, useEffect} from "react";
+import axios from "axios";
+function ChatPage() {
+
+    const [chats, setChats] = useState([]);
+    
+    const fetchChat = async () => {
+        const { data } = await axios.get("/api/chat");
+        console.log("data", data);
+        setChats(data);
+    }
+    useEffect(() => {
+        fetchChat();
+    }, []);
+
+  return (
+    <>
+         <div>
+           
+            {chats.map((chat) => (
+                <div>{chat.chatName}</div>
+            ))}
+        </div>
+    </>
+  );
+}
+
+export default ChatPage
